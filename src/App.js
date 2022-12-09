6b9f0f2b61af8e1ae228877b30f5130c10be74c2
yash kumar mishra <mishrayash3778@gmail.com>
2022-12-09 19:43:08 +0530

    hdwi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css"
+import {useSelector, useDispatch} from "react-redux";
+import {incNumber,decNumber} from "./actions/index"
+
 const App = () => {
+const myState = useSelector((state) => state.Changethenumber);
+const dispatch = useDispatch();
+
     return (
         <div>
             <h1>Increament and decreament counter using redux</h1>
-            <input name="quantity" type="text" value="0"></input>
-            <button>+</button>
-            <button>-</button>
+            <input name="quantity" type="text" value={myState}></input>
+            <button onClick={ () => dispatch(incNumber())}>+</button>
+            <button  onClick={() => dispatch(decNumber())}>-</button>
         </div>
     )
 }
